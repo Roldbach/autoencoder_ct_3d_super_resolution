d41ad07da69a25b5cb15d1c0502d5791d04744c4
Roldbach <roldbach@yahoo.com>
2023-02-25 23:40:17 +0000

    Add dataset_configuration.py

---
+++ configuration/dataset_configuration.py
@@ -0,0 +1,9 @@
+"""Dataset-related configurations.
+
+This module contains configurations about the dataset used
+in the experiment. 
+"""
+
+
+# File path of the dataset loading file
+DATASET_LOADING_FILE_PATH = ''
