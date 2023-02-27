58f253916ca0c7da99bc7134d1c351f433253f2c
Roldbach <roldbach@yahoo.com>
2023-02-27 21:02:24 +0000

    docs(device_configuration.py): Add detailed docs
    Add detailed documentations to explain the usage of xx_DEVICE_ID

--- configuration/device_configuration.py
+++ configuration/device_configuration.py
@@ -9,10 +9,12 @@ import torch
 
 # Device used in model training
 TRAIN_DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-# Device ID used in model training (if multiple GPU available)
+# Device ID used in model training
+# None if 0/1 GPU is available, otherwise list all device IDs
 TRAIN_DEVICE_ID = (0, 1)
 
 # Device used in model inference
 TEST_DEVICE = torch.device('cpu')
 # Device ID used in model inference
+# None if 0/1 GPU is available, otherwise list all device IDs
 TEST_DEVICE_ID = None
