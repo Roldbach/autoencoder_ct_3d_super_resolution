1feef9ec82d1620b560ce895d9e89e41fd7c00a6
Roldbach <roldbach@yahoo.com>
2023-02-27 23:36:52 +0000

    refactor(train.py): Allow command-line interaction
    Refactor train.py
    so users can interact with it using the terminal directly

--- train.py
+++ train.py
@@ -17,24 +17,86 @@ model, please specify the following experiment settings:
         /upsampling in the z-axis.
     (9) learning_rate: float, the step size in gradient
         updating.
-    (10) window: tuple[float|None, float|None], the range
-        of HU values interested.
+    (10) window: tuple[int|None, int|None], the range of HU
+        values interested.
 """
+import argparse
+import sys
+
 from helper import train_helper
 
+def main() -> int:
+    """Runs the main script."""
+    train_helper.train(parse_argument())
 
-if __name__ == '__main__':
-    delegate = train_helper.TrainDelegate(
-        model_name = 'PlainCNN',
-        upsample_name = 'same_insertion',
-        scale_factor = 4,
-        is_resuming = True,
-        output_directory_path = './temp',
-        epoch = 500,
-        batch_size = 16,
-        patch_size = 64,
-        learning_rate = 1e-5,
-        window = (-1024, 1476),
+    return 0
+
+def parse_argument() -> argparse.Namespace:
+    """Parse arguments from the terminal."""
+    parser = argparse.ArgumentParser(description=globals()['__doc__'])
+
+    parser.add_argument(
+        '--model_name',
+        type = str,
+        required = True,
+        help = 'The name of the model: PlainCNN | AE_Maxpool | AE_Conv | UNet',
+    )
+    parser.add_argument(
+        '--upsample_name',
+        type = str,
+        required = True,
+        help = 'The name of the upsampling method: trilinear_interpolation | same_insertion',
+    )
+    parser.add_argument(
+        '--output_directory_path',
+        type = str,
+        required = True,
+        help = 'The directory to store model weights and record',
+    )
+    parser.add_argument(
+        '--is_resuming',
+        action = 'store_true',
+        help = 'Whether to resume from the last experiment',
+    )
+    parser.add_argument(
+        '--epoch',
+        type = int,
+        required = True,
+        help = 'The number of epochs',
+    )
+    parser.add_argument(
+        '--batch_size',
+        type = int,
+        required = True,
+        help = 'The number of data in one batch',
+    )
+    parser.add_argument(
+        '--patch_size',
+        type = int,
+        required = True,
+        help = 'The size of cubic patches',
+    )
+    parser.add_argument(
+        '--scale_factor',
+        type = int,
+        required = True,
+        help = 'The scale factor of downsampling/upsampling in the z-axis',
+    )
+    parser.add_argument(
+        '--learning_rate',
+        type = float,
+        required = True,
+        help = 'The step size in gradient updating',
+    )
+    parser.add_argument(
+        '--window',
+        type = int,
+        nargs = '+',
+        default = (None, None),
+        help = 'The range of HU values interested'
     )
 
-    train_helper.train(delegate)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    sys.exit(main())
