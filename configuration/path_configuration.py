963d94a33cbd1b17188dac1bc6588cb458be7697
Roldbach <roldbach@yahoo.com>
2023-02-27 21:04:31 +0000

    style(path_configuration.py): Delete unused paths
    Delete unused paths for cleaner codes.

--- configuration/path_configuration.py
+++ configuration/path_configuration.py
@@ -3,12 +3,7 @@
 This module contains configurations about the paths used
 in the experiment. 
 """
-import os
 
 
-# Path of the project root directory
-ROOT_DIRECTORY_PATH = os.getcwd()
 # Path of the dataset loading file
 DATASET_LOADING_FILE_PATH = ''
-# Path of the directory storing all pre-trained model weigths
-WEIGHT_DIRECTORY_PATH = f'{ROOT_DIRECTORY_PATH}/weight'
