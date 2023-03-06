54a6352f2e62ae231ccead7983fb2966c4350d2c
Roldbach <roldbach@yahoo.com>
2023-03-06 20:30:52 +0000

    docs(train.py): delete repeated docs

--- train.py
+++ train.py
@@ -1,26 +1,4 @@
 """Script for model training.
-
-This is the script used for model training. To train a
-model, please specify the following experiment settings:
-    (1) model_name: str, one of PlainCNN/AE_Maxpool/AE_Conv/
-        UNet.
-    (2) upsample_name: str, one of trilinear_interpolation/
-        same_insertion.
-    (3) output_directory_path: str, the directory to store
-        model weights and record.
-    (4) is_resuming: bool, whether resuming from the last
-        experiment.
-    (5) epoch: int, the number of epochs.
-    (6) batch_size: int, the number of data in one batch.
-    (7) patch_size: int, the number of cubic patches.
-    (8) scale_factor: int, the scale factor of downsampling
-        /upsampling in the z-axis.
-    (9) learning_rate: float, the step size in gradient
-        updating.
-    (10) window: tuple[int|None, int|None], the range of HU
-        values interested.
-
-This script can be interacted directly from the terminal.
 """
 import argparse
 import sys
@@ -35,7 +13,7 @@ def main() -> int:
     return 0
 
 def parse_argument() -> argparse.Namespace:
-    """Parse arguments from the terminal."""
+    """Parses arguments from the terminal."""
     parser = argparse.ArgumentParser(description=globals()['__doc__'])
 
     parser.add_argument(
