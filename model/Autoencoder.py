a34b993fbd0786863875c504f0004aab9e73554f
Roldbach <roldbach@yahoo.com>
2023-03-25 22:01:08 +0000

    upload Autoencoder.py

--- model/Autoencoder.py
+++ model/Autoencoder.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 
-class Autoencoder_Maxpool(nn.Module):
+class AutoencoderMaxpool(nn.Module):
     """AE-Maxpool model.
 
     This model is built upon the baseline model(Plain CNN).
@@ -16,19 +16,24 @@ class Autoencoder_Maxpool(nn.Module):
         super().__init__()
 
         self._encoder = nn.Sequential(
-            DoubleConv3DBlock(1, 64),
+            Conv3DBlock(1, 64),
+            Conv3DBlock(64, 64),
             nn.MaxPool3d(kernel_size=2, stride=2, padding=0, dilation=1),
-            DoubleConv3DBlock(64, 128),
+            Conv3DBlock(64, 128),
+            Conv3DBlock(128, 128),
             nn.MaxPool3d(kernel_size=2, stride=2, padding=0, dilation=1),
-            DoubleConv3DBlock(128, 256),
+            Conv3DBlock(128, 256),
+            Conv3DBlock(256, 256),
         )
 
         self._decoder = nn.Sequential(
-            DoubleConv3DBlock(256, 256),
-            TrilinearInterpolationLayer(),
-            DoubleConv3DBlock(256, 128),
-            TrilinearInterpolationLayer(),
+            Conv3DBlock(256, 256),
+            Conv3DBlock(256, 128),
+            TrilinearInterpolationConv3DBlock(128, 128),
+            Conv3DBlock(128, 128),
             Conv3DBlock(128, 64),
+            TrilinearInterpolationConv3DBlock(64, 64),
+            Conv3DBlock(64, 64),
             Conv3DBlock(64, 1),
         )
     
@@ -41,7 +46,7 @@ class Autoencoder_Maxpool(nn.Module):
 
         return input + residual
 
-class Autoencoder_Conv(nn.Module):
+class AutoencoderConv(nn.Module):
     """AE-Conv model.
 
     This model is built upon the baseline model(Plain CNN).
@@ -55,19 +60,24 @@ class Autoencoder_Conv(nn.Module):
         super().__init__()
 
         self._encoder = nn.Sequential(
-            DoubleConv3DBlock(1, 64),
-            StrideConv3DBlock(64, 128),
-            DoubleConv3DBlock(128, 128),
-            StrideConv3DBlock(128, 256),
-            DoubleConv3DBlock(256, 256),
+            Conv3DBlock(1, 64),
+            Conv3DBlock(64, 64),
+            StrideConv3DBlock(64, 64),
+            Conv3DBlock(64, 128),
+            Conv3DBlock(128, 128),
+            StrideConv3DBlock(128, 128),
+            Conv3DBlock(128, 256),
+            Conv3DBlock(256, 256),
         )
 
         self._decoder = nn.Sequential(
-            DoubleConv3DBlock(256, 256),
-            TrilinearInterpolationLayer(),
-            DoubleConv3DBlock(256, 128),
-            TrilinearInterpolationLayer(),
+            Conv3DBlock(256, 256),
+            Conv3DBlock(256, 128),
+            TrilinearInterpolationConv3DBlock(128, 128),
+            Conv3DBlock(128, 128),
             Conv3DBlock(128, 64),
+            TrilinearInterpolationConv3DBlock(64, 64),
+            Conv3DBlock(64, 64),
             Conv3DBlock(64, 1),
         )
     
@@ -90,22 +100,7 @@ class Conv3DBlock(nn.Module):
             nn.Conv3d(
                 channel_in, channel_out, kernel_size=3, stride=1, padding='same',
             ),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
-        )
-    
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        """Returns output by forward calculation."""
-        return self._layer(input)
-
-class DoubleConv3DBlock(nn.Module):
-    """[Conv3D + LeakyReLU (negative_slope=0.1)] * 2"""
-    def __init__(self, channel_in: int, channel_out: int) -> None:
-        """Initialises DoubleConv3DBlock."""
-        super().__init__()
-
-        self._layer = nn.Sequential(
-            Conv3DBlock(channel_in, channel_out),
-            Conv3DBlock(channel_out, channel_out),
+            nn.LeakyReLU(negative_slope=0.1),
         )
     
     def forward(self, input: torch.Tensor) -> torch.Tensor:
@@ -129,13 +124,16 @@ class StrideConv3DBlock(nn.Module):
         """Returns output by forward calculation."""
         return self._layer(input)
 
-class TrilinearInterpolationLayer(nn.Module):
-    """Trilinear Interpolation (scale_factor=2)"""
-    def __init__(self) -> None:
+class TrilinearInterpolationConv3DBlock(nn.Module):
+    """Trilinear Interpolation (scale_factor=2) + Conv3D (kernel_size=1)"""
+    def __init__(self, channel_in: int, channel_out: int) -> None:
         """Initialises TrilinearInterpolationLayer."""
         super().__init__()
+
+        self._layer = nn.Conv3d(
+            channel_in, channel_out, kernel_size=1, stride=1, padding='same')
     
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         """Returns output by forward calculation."""
-        return nn.functional.interpolate(
-            input, scale_factor=2, mode='trilinear')
+        return self._layer(
+            nn.functional.interpolate(input, scale_factor=2, mode='trilinear'))
