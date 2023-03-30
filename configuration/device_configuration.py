34eaf11d927e877da48c09f52561e55a2ecc22dd
Roldbach <roldbach@yahoo.com>
2023-03-30 18:26:39 +0100

    feat(device_configuration.py): detect device ID

--- configuration/device_configuration.py
+++ configuration/device_configuration.py
@@ -10,11 +10,9 @@ import torch
 # Device used in model training
 TRAIN_DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 # Device ID used in model training
-# None if 0/1 GPU is available, otherwise list all device IDs
-TRAIN_DEVICE_ID = (0, 1)
+TRAIN_DEVICE_ID = [i for i in range(torch.cuda.device_count())]
 
 # Device used in model inference
 TEST_DEVICE = torch.device('cpu')
 # Device ID used in model inference
-# None if 0/1 GPU is available, otherwise list all device IDs
-TEST_DEVICE_ID = None
+TEST_DEVICE_ID = []
